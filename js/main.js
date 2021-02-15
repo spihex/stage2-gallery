03de78d14d87578bc67aa233d1481c3b62ca9582
spihex <33398695+spihex@users.noreply.github.com>
2021-02-15 12:43:01 +1000

    теперь с классом

--- js/main.js
+++ js/main.js
@@ -1,117 +1,226 @@
+class Gallery {
+
+    constructor(options) {
+
+        this.itemsContainer = document.querySelector(options.container + ' .gallery__items');
+        this.galleryTitle = document.querySelector(options.container + ' .gallery__title');
+        this.buttonLoadMore = document.querySelector(options.container + ' .button__load-more');
+        this.buttonPrev = document.querySelector(options.container + ' .button__prev');
+        this.buttonNext = document.querySelector(options.container + ' .button__next');
+
+        this.photos;
+
+        this.currentAlbumID = options.id ? options.id : 1;
+
+        this.photosStartLoadingOffset = 10;
+        this.galleryItemsToLoad = 10;
+
+        this.buttonPrev.addEventListener('click', this.prev.bind(this));
+        this.buttonNext.addEventListener('click', this.next.bind(this));
+        this.buttonLoadMore.addEventListener('click', this.loadMore.bind(this));
+
+        this.init();
+
+    }
+
+
+    init() {
+        this.getPhotosByAlbumID(this.currentAlbumID);
+    }
+
+    changeGalleryTitleByAlbumID(str, albumID) {
+        fetch('https://jsonplaceholder.typicode.com/albums?id=' + albumID)
+            .then(response => response.json())
+            .then(album => {
+                this.galleryTitle.innerHTML = str + album[0].title;
+            })
+    }
+
+    addAlbumItemToGallery(photos, startAtPos, endAtPos) {
+        for (let i = startAtPos; i < startAtPos + endAtPos; i++) {
+            if (photos[i]) {
+                let HTMLtemplate = `
+            <div class="gallery__item"> 
+                <span>${photos[i].id}</span>
+                <img class="gallery__item-image" data-full-size="${photos[i].url}" src="${photos[i].thumbnailUrl}" >
+            </div>
+             `;
+                this.itemsContainer.innerHTML += HTMLtemplate;
+            }
+        }
+    }
+
+    getPhotosByAlbumID(ID) {
+        fetch('https://jsonplaceholder.typicode.com/photos?albumId=' + ID)
+            .then(response => {
+
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('С запросом что-то не так');
+                }
+
+            })
+            .then(albumJSON => {
+
+                if (albumJSON.length > 0) {
+
+                    this.photos = albumJSON;
+                    this.itemsContainer.innerHTML = '';
+                    this.photosStartLoadingOffset = 10;
+
+                    this.changeGalleryTitleByAlbumID(`Заголовок альбома №${ID}: `, ID);
+                    this.addAlbumItemToGallery(this.photos, 0, this.galleryItemsToLoad);
+
+                } else {
+                    this.currentAlbumID = 1;
+                    throw new Error('Альбом не найден');
+                }
+
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    }
+
+
+
+    loadMore() {
+        this.addAlbumItemToGallery(this.photos, this.photosStartLoadingOffset, this.galleryItemsToLoad);
+        this.photosStartLoadingOffset *= 2;
+    }
+
+    prev() {
+        this.getPhotosByAlbumID(--this.currentAlbumID);
+    }
+
+    next() {
+
+        this.getPhotosByAlbumID(++this.currentAlbumID);
+    }
+
+
+}
+
+////#region 
+
 // ALBUM LOAD & NAVIGATE
-let galleryItemsContainer = document.querySelector('.gallery__items');
-let galleryTitle = document.querySelector('.gallery__title');
 
-let photos;
-let currentAlbumID = 1;
-let photosStartLoadingOffset = 10;
-let galleryItemsToLoad = 10;
+// let galleryItemsContainer = document.querySelector('.gallery__items');
+// let galleryTitle = document.querySelector('.gallery__title');
 
-let galleryButtonLoadMore = document.querySelector('.button__load-more');
-let galleryNavButtons = document.querySelectorAll('.gallery__nav-button');
+// let photos;
+// let currentAlbumID = 1;
+// let photosStartLoadingOffset = 10;
+// let galleryItemsToLoad = 10;
 
-document.addEventListener("DOMContentLoaded", function () {
-    getPhotosByAlbumID(currentAlbumID);
-});
+// let galleryButtonLoadMore = document.querySelector('.button__load-more');
+// let galleryNavButtons = document.querySelectorAll('.gallery__nav-button');
 
-galleryButtonLoadMore.addEventListener("click", function (e) {
-    e.preventDefault();
-    addAlbumItemToGallery(photos, photosStartLoadingOffset, galleryItemsToLoad);
-    photosStartLoadingOffset *= 2;
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     getPhotosByAlbumID(currentAlbumID);
+// });
 
-galleryNavButtons.forEach(button => {
-    button.addEventListener('click', e => {
-        e.preventDefault();
-        if (e.target.classList.contains('button__next')) {
-            currentAlbumID++
-        } else {
-            currentAlbumID--
-        }
-        getPhotosByAlbumID(currentAlbumID);
-    })
-})
+// galleryButtonLoadMore.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     addAlbumItemToGallery(photos, photosStartLoadingOffset, galleryItemsToLoad);
+//     photosStartLoadingOffset *= 2;
+// });
 
-function getPhotosByAlbumID(ID) {
-    fetch('https://jsonplaceholder.typicode.com/photos?albumId=' + ID)
-        .then(response => {
+// galleryNavButtons.forEach(button => {
+//     button.addEventListener('click', e => {
+//         e.preventDefault();
+//         if (e.target.classList.contains('button__next')) {
+//             currentAlbumID++
+//         } else {
+//             currentAlbumID--
+//         }
+//         getPhotosByAlbumID(currentAlbumID);
+//     })
+// })
 
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('С запросом что-то не так');
-            }
+// function getPhotosByAlbumID(ID) {
+//     fetch('https://jsonplaceholder.typicode.com/photos?albumId=' + ID)
+//         .then(response => {
 
-        })
-        .then(albumJSON => {
+//             if (response.ok) {
+//                 return response.json();
+//             } else {
+//                 throw new Error('С запросом что-то не так');
+//             }
 
-            if (albumJSON.length > 0) {
+//         })
+//         .then(albumJSON => {
 
-                photos = albumJSON;
-                galleryItemsContainer.innerHTML = '';
-                photosStartLoadingOffset = 10;
+//             if (albumJSON.length > 0) {
 
-                changeGalleryTitleByAlbumID(`Заголовок альбома №${ID}: `, ID);
-                addAlbumItemToGallery(photos, 0, galleryItemsToLoad);
+//                 photos = albumJSON;
+//                 galleryItemsContainer.innerHTML = '';
+//                 photosStartLoadingOffset = 10;
 
-            } else {
-                currentAlbumID = 1;
-                throw new Error('Альбом не найден');
-            }
+//                 changeGalleryTitleByAlbumID(`Заголовок альбома №${ID}: `, ID);
+//                 addAlbumItemToGallery(photos, 0, galleryItemsToLoad);
 
-        })
-        .catch((error) => {
-            alert(error);
-        });
-}
+//             } else {
+//                 currentAlbumID = 1;
+//                 throw new Error('Альбом не найден');
+//             }
 
-function addAlbumItemToGallery(photos, startAtPos, endAtPos) {
+//         })
+//         .catch((error) => {
+//             alert(error);
+//         });
+// }
 
-    for (let i = startAtPos; i < startAtPos + endAtPos; i++) {
-        if (photos[i]) {
-            let HTMLtemplate = `
-        <div class="gallery__item"> 
-            <span>${photos[i].id}</span>
-            <img class="gallery__item-image" data-full-size="${photos[i].url}" src="${photos[i].thumbnailUrl}" >
-        </div>
-         `;
-            galleryItemsContainer.innerHTML += HTMLtemplate;
-        }
-    }
-}
+// function addAlbumItemToGallery(photos, startAtPos, endAtPos) {
 
-function changeGalleryTitleByAlbumID(str, albumID) {
-    fetch('https://jsonplaceholder.typicode.com/albums?id=' + albumID)
-        .then(response => response.json())
-        .then(album => {
-            galleryTitle.innerHTML = str + album[0].title;
-        })
-}
+//     for (let i = startAtPos; i < startAtPos + endAtPos; i++) {
+//         if (photos[i]) {
+//             let HTMLtemplate = `
+//         <div class="gallery__item"> 
+//             <span>${photos[i].id}</span>
+//             <img class="gallery__item-image" data-full-size="${photos[i].url}" src="${photos[i].thumbnailUrl}" >
+//         </div>
+//          `;
+//             galleryItemsContainer.innerHTML += HTMLtemplate;
+//         }
+//     }
+// }
 
+// function changeGalleryTitleByAlbumID(str, albumID) {
+//     fetch('https://jsonplaceholder.typicode.com/albums?id=' + albumID)
+//         .then(response => response.json())
+//         .then(album => {
+//             galleryTitle.innerHTML = str + album[0].title;
+//         })
+// }
+////#endregion
 // ALBUM LIGHTBOX
-let lightbox = document.createElement('div');
-lightbox.classList.add('lightbox');
 
-document.body.appendChild(lightbox);
-lightbox.addEventListener('click', e => {
-    lightbox.classList.remove('active')
-})
 
-galleryItemsContainer.addEventListener('click', e => {
-    if (e.target.classList.contains('gallery__item-image')) {
-        lightbox.classList.add('active')
-        let img = document.createElement('img')
-        img.src = e.target.getAttribute('data-full-size');
-        while (lightbox.firstChild) {
-            lightbox.removeChild(lightbox.firstChild)
-        }
-        lightbox.appendChild(img)
-    }
-});
+// let lightbox = document.createElement('div');
+// lightbox.classList.add('lightbox');
 
-document.addEventListener("keydown", function (event) {
-    let key = event.key;
-    if (key === "Escape") {
-        lightbox.classList.remove('active');
-    }
-});
+// document.body.appendChild(lightbox);
+// lightbox.addEventListener('click', e => {
+//     lightbox.classList.remove('active')
+// })
+
+// galleryItemsContainer.addEventListener('click', e => {
+//     if (e.target.classList.contains('gallery__item-image')) {
+//         lightbox.classList.add('active')
+//         let img = document.createElement('img')
+//         img.src = e.target.getAttribute('data-full-size');
+//         while (lightbox.firstChild) {
+//             lightbox.removeChild(lightbox.firstChild)
+//         }
+//         lightbox.appendChild(img)
+//     }
+// });
+
+// document.addEventListener("keydown", function (event) {
+//     let key = event.key;
+//     if (key === "Escape") {
+//         lightbox.classList.remove('active');
+//     }
+// });
